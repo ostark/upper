3f2c1103536880d2a07603ec3d326d1a31d7219a
John Lennard <john@yakmoo.se>
2023-02-02 14:49:52 +1300

    update default config

--- src/config.example.php
+++ src/config.example.php
@@ -3,11 +3,12 @@
  * Don't edit the config.example.php.
  * Instead modify the projects/config/upper.php and use ENV VARS
  */
+use craft\helpers\App;
 
 return [
 
     // Which driver?
-    'driver'        => getenv('UPPER_DRIVER') ?: 'dummy',
+    'driver'        => App::env('UPPER_DRIVER') ?: 'dummy',
 
     // Default for Cache-control s-maxage
     'defaultMaxAge' => 3600 * 24 * 7,
@@ -20,7 +21,7 @@ return [
     // same cache store for several Craft installations.
     // Keep it nice and short for the sake of readability when debugging.
     // 1-8 characters, special chars get removed
-    'keyPrefix'     => getenv('UPPER_KEY_PREFIX') ?: '',
+    'keyPrefix'     => App::env('UPPER_KEY_PREFIX') ?: '',
 
     // Optional maximum length for the cache tag header. Setting this higher will
     // allow Upper to return more tags in the header. However, it will also require
@@ -36,38 +37,38 @@ return [
         'varnish'    => [
             'tagHeaderName'   => 'XKEY',
             'purgeHeaderName' => 'XKEY-PURGE',
-            'purgeUrl'        => getenv('VARNISH_URL') ?: 'http://127.0.0.1:80/',
-            'headers'         => getenv('VARNISH_HOST') ? ['Host' => getenv('VARNISH_HOST')] : [],
+            'purgeUrl'        => App::env('VARNISH_URL') ?: 'http://127.0.0.1:80/',
+            'headers'         => App::env('VARNISH_HOST') ? ['Host' => App::env('VARNISH_HOST')] : [],
             'softPurge'       => false,
         ],
 
         // Fastly config
         'fastly'     => [
             'tagHeaderName' => 'Surrogate-Key',
-            'serviceId'     => getenv('FASTLY_SERVICE_ID'),
-            'apiToken'      => getenv('FASTLY_API_TOKEN'),
-            'domain'        => getenv('FASTLY_DOMAIN'),
+            'serviceId'     => App::env('FASTLY_SERVICE_ID'),
+            'apiToken'      => App::env('FASTLY_API_TOKEN'),
+            'domain'        => App::env('FASTLY_DOMAIN'),
             'softPurge'     => false
         ],
 
         // KeyCDN config
         'keycdn'     => [
             'tagHeaderName' => 'Cache-Tag',
-            'apiKey'        => getenv('KEYCDN_API_KEY'),
-            'zoneId'        => getenv('KEYCDN_ZONE_ID'),
-            'zoneUrl'       => getenv('KEYCDN_ZONE_URL')
+            'apiKey'        => App::env('KEYCDN_API_KEY'),
+            'zoneId'        => App::env('KEYCDN_ZONE_ID'),
+            'zoneUrl'       => App::env('KEYCDN_ZONE_URL')
         ],
 
         // CloudFlare config
         'cloudflare' => [
             'tagHeaderName'      => 'Cache-Tag',
             'tagHeaderDelimiter' => ',',
-            'apiToken'           => getenv('CLOUDFLARE_API_TOKEN'),
-            'zoneId'             => getenv('CLOUDFLARE_ZONE_ID'),
-            'domain'             => getenv('CLOUDFLARE_DOMAIN'),
+            'apiToken'           => App::env('CLOUDFLARE_API_TOKEN'),
+            'zoneId'             => App::env('CLOUDFLARE_ZONE_ID'),
+            'domain'             => App::env('CLOUDFLARE_DOMAIN'),
             // deprecated, do not use for new installs
-            'apiKey'             => getenv('CLOUDFLARE_API_KEY'),
-            'apiEmail'           => getenv('CLOUDFLARE_API_EMAIL'),
+            'apiKey'             => App::env('CLOUDFLARE_API_KEY'),
+            'apiEmail'           => App::env('CLOUDFLARE_API_EMAIL'),
         ],
 
         // Dummy driver (default)
